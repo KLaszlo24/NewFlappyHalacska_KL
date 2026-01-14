02eb1ab4e17e32ea5a73f091104efc9044d3e30b
KLaszlo24 <krajczlaci234@gmail.com>
2026-01-14 09:12:16 +0100

    2. StartGame()

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace NewFlappyHalacska
 {
@@ -20,9 +21,73 @@ namespace NewFlappyHalacska
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        DispatcherTimer timer=new DispatcherTimer();
+        
+        double score;
+        int gravitacio;
+        bool jatekvege;
+        Rect FlappyhalHitbox;
         public MainWindow()
         {
             InitializeComponent();
+            timer.Tick += MainEventTimer;
+            timer.Interval = TimeSpan.FromMilliseconds(20);
+            StartGame();
+        }
+
+        private void MainEventTimer(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void StartGame()
+        {
+            MyCanvas.Focus();
+
+            int temp = 300;
+            score = 0;
+
+            jatekvege= false;
+            Canvas.SetTop(FlappyHal, 190);
+
+            foreach(var x in MyCanvas.Children.OfType<Image>())
+            {
+                if((string)x.Tag == "obs1")
+                {
+                    Canvas.SetLeft(x, 500);
+                }
+                if ((string)x.Tag == "obs2")
+                {
+                    Canvas.SetLeft(x, 800);
+                }
+                if ((string)x.Tag == "obs3")
+                {
+                    Canvas.SetLeft(x, 1100);
+                }
+                if ((string)x.Tag == "cloud")
+                {
+                    Canvas.SetLeft(x, 300 + temp);
+                    temp = 800;
+                }
+            }
+
+            timer.Start();
+        }
+
+        private void EndGame()
+        {
+
         }
     }
 }
